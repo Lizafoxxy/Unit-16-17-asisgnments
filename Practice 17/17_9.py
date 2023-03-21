b3b024fdcc893fd8fd50ad0e85f96799b8665657
Lizafoxxy <lizzon@mail.ru>
2023-03-21 16:57:45 +0300

    binary_search function amended

--- Practice 17/17_9.py
+++ Practice 17/17_9.py
@@ -7,23 +7,23 @@ def sorting(array):   # сортировка пузырьком
                 array[j], array[j + 1] = array[j + 1], array[j]
     return array
 
-def binary_search(L, element, left, right):
+def binary_search(array1, element, left, right):
     if left > right:  # если левая граница превысила правую,
         return False  # значит элемент отсутствует
+    else:
+        middle = (right + left) // 2  # находимо середину
+        if element == array1[middle]:  # СЕГМЕНТ 1
+            return middle-1  # возвращаем  индекс элемента предшествуюшего середине
+        elif element < array1[middle] and element <= array1[middle-1]:  # СЕГМЕНТ 2
+            return binary_search(array1, element, left, middle-1)
+        elif element < array1[middle] and element > array1[middle-1]: # СЕГМЕНТ 3
+            return middle-1
+        elif element > array1[middle] and element <= array1[middle+1]: # СЕГМЕНТ 4
+            return middle
+        elif element > array1[middle] and element > array1[middle+1]: # СЕГМЕНТ 5
+            return binary_search(array1, element, middle+1, right)
 
-    middle = (right + left) // 2  # находимо середину
-    if element == L[middle]:  # если элемент в середине,
-        return middle-1  # возвращаем этот индекс
-    elif element < L[middle] and element < L[middle-1]:  # если элемент меньше элемента в середине
-                                # и меньше предыдущего перед серединой, то рекурсивно ищем в
-                                # левой половине
-        return binary_search(L, element, left, middle-1)
-    elif element > L[middle] and element <= L[middle+1]:
-        return middle
-    elif element > L[middle] and element > L[middle+1]:
-        return binary_search(L, element, middle+1, right)
-
-print("Введите последовательность целых чисел через пробел: ")
+print("Введите последовательность неповторяющихся целых чисел через пробел: ")
 num_str = sys.stdin.readline()
 num_str = num_str.replace('\n', '')
 str_list = list(num_str.split(sep=' '))
@@ -34,21 +34,29 @@ try:
 except ValueError as e:
     print(f"Не все введенные Вами данные являются числами. Повторите попытку запуска программы.")
 else:
-    print("Вы ввели корректные данные.")
+    print("Вы ввели последовательность чисел.")
+
+# проверка на отсутствие повторяющихся элементов
+set_num_list = set(num_list)
+if len(num_list) != len(set_num_list):
+    print("Во введенной последовательности есть повторяющиеся числа. Повторите попытку запуска программы.")
+    quit()
 
 sorted_list = sorting(num_list)
 
-print("Введите любое число: ")
+print("Введите любое целое число: ")
 num = sys.stdin.readline()
+# проверка, что пользователем введено целое число
 try:
     num = int(num)
 except ValueError as err:
     print(f"Вы ввели не число. Повторите попытку запуска программы.")
+    quit()
 else:
     print("Вы ввели корректные данные.")
 
 
-print(f"Ваши числа в отсортированном порядке: {sorted_list}.")
+print(f"Числовой ряд из Ваших чисел в отсортированном порядке: {sorted_list}.")
 
 searched_element = binary_search(sorted_list, num, 0, len(sorted_list)-1)
-print(f"Номер позиции элемента, который меньше введенного Вами числа, а следующий за ним больше или равен этому числу: {searched_element}")
+print(f"Номер позиции элемента числового ряда, который меньше введенного Вами числа, а следующий за ним больше или равен этому числу: {searched_element}")
